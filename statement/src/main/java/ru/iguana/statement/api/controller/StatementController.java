6efaf03f8d9aabeb662ea510b2c0f5507abbff99
Iguan04ka <Iguana_2004@mail.ru>
2024-12-21 18:33:54 +0400

    created a structure of microservice

---
+++ statement/src/main/java/ru/iguana/statement/api/controller/StatementController.java
@@ -0,0 +1,8 @@
+package ru.iguana.statement.api.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatementController {
+}
---
+++ statement/src/main/java/ru/iguana/statement/api/service/StatementService.java
@@ -0,0 +1,9 @@
+package ru.iguana.statement.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatementService {
+
+
+}
