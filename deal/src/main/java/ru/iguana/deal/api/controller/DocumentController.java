c0196160a4abe3dcf1b725dabde21a028599e66a
Iguan04ka <Iguana_2004@mail.ru>
2025-01-05 22:15:42 +0400

    wrote logs for deal

--- deal/src/main/java/ru/iguana/deal/api/controller/DocumentController.java
+++ deal/src/main/java/ru/iguana/deal/api/controller/DocumentController.java
@@ -1,6 +1,7 @@
 package ru.iguana.deal.api.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.iguana.deal.api.dto.EmailMessageDto;
 import ru.iguana.deal.api.service.DocumentService;
@@ -16,22 +17,26 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/deal/document/{statementId}")
 @AllArgsConstructor
+@Slf4j
 public class DocumentController {
 
     private final DocumentService documentService;
 
     @PostMapping("/send")
-    public void send(@PathVariable UUID statementId){
+    public void send(@PathVariable UUID statementId) {
+        log.info("Received request to send documents for statementId: {}", statementId);
         documentService.sendDocuments(statementId);
     }
 
     @PostMapping("/sign")
-    public void sign(@PathVariable UUID statementId){
+    public void sign(@PathVariable UUID statementId) {
+        log.info("Received request to sign documents for statementId: {}", statementId);
         documentService.signDocuments(statementId);
     }
 
     @PostMapping("/code")
-    public void code(@PathVariable UUID statementId){
+    public void code(@PathVariable UUID statementId) {
+        log.info("Received request to send SES code for statementId: {}", statementId);
         documentService.codeDocuments(statementId);
     }
 }
--- deal/src/main/java/ru/iguana/deal/api/service/DocumentService.java
+++ deal/src/main/java/ru/iguana/deal/api/service/DocumentService.java
@@ -1,6 +1,7 @@
 package ru.iguana.deal.api.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.iguana.deal.api.dto.EmailMessageDto;
 import ru.iguana.deal.kafka.KafkaProducer;
@@ -14,6 +15,7 @@ import java.util.UUID;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class DocumentService {
 
     private final KafkaProducer kafkaProducer;
@@ -22,41 +24,59 @@ public class DocumentService {
 
     private final ClientRepository clientRepository;
 
-    public void sendDocuments(UUID statementId){
+    public void sendDocuments(UUID statementId) {
+        log.info("Preparing to send documents for statementId: {}", statementId);
         EmailMessageDto message = new EmailMessageDto()
                 .setAddress(getEmailFromClient(statementId))
                 .setTheme(EmailTheme.SEND_DOCUMENTS)
                 .setStatementId(statementId)
                 .setText("Перейти к оформлению документов");
 
+        log.info("Sending message to 'send-documents' topic");
+        log.debug("Sending message to 'send-documents' topic: {}", message);
         kafkaProducer.sendMessageToSendDocumentsTopic(message);
     }
 
-    public void signDocuments(UUID statementId){
+    public void signDocuments(UUID statementId) {
+        log.info("Preparing to sign documents for statementId: {}", statementId);
         EmailMessageDto message = new EmailMessageDto()
                 .setAddress(getEmailFromClient(statementId))
                 .setTheme(EmailTheme.CREATE_DOCUMENTS)
                 .setStatementId(statementId)
                 .setText("Документы сформированы");
-
+        log.info("Sending message to 'create-documents' topic");
+        log.debug("Sending message to 'create-documents' topic: {}", message);
         kafkaProducer.sendMessageToCreateDocumentsTopic(message);
     }
 
-    public void codeDocuments(UUID statementId){
+    public void codeDocuments(UUID statementId) {
+        log.info("Preparing to send SES code for statementId: {}", statementId);
         EmailMessageDto message = new EmailMessageDto()
                 .setAddress(getEmailFromClient(statementId))
                 .setTheme(EmailTheme.SEND_SES)
                 .setStatementId(statementId)
                 .setText("Подпишите документы");
 
+        log.info("Sending message to 'send-ses' topic");
+        log.debug("Sending message to 'send-ses' topic: {}", message);
         kafkaProducer.sendMessageToSendSesTopic(message);
     }
 
-    private String getEmailFromClient(UUID statementId){
-        Statement statement = statementRepository.findById(statementId).orElseThrow();
+    private String getEmailFromClient(UUID statementId) {
+        log.info("Fetching email for statementId: {}", statementId);
+        Statement statement = statementRepository.findById(statementId)
+                .orElseThrow(() -> {
+                    log.error("Statement not found for id: {}", statementId);
+                    return new RuntimeException("Statement not found");
+                });
 
-        Client client = clientRepository.findById(statement.getClientId()).orElseThrow();
+        Client client = clientRepository.findById(statement.getClientId())
+                .orElseThrow(() -> {
+                    log.error("Client not found for id: {}", statement.getClientId());
+                    return new RuntimeException("Client not found");
+                });
 
+        log.debug("Retrieved email: {} for statementId: {}", client.getEmail(), statementId);
         return client.getEmail();
     }
 }
