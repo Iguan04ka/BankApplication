8fba1aa606446767b8871644fe10e08c0c6c143f
Iguan04ka <Iguana_2004@mail.ru>
2025-01-05 22:41:17 +0400

    created swagger doc for api's in DocumentController

--- deal/src/main/java/ru/iguana/deal/api/controller/DocumentController.java
+++ deal/src/main/java/ru/iguana/deal/api/controller/DocumentController.java
@@ -1,16 +1,11 @@
 package ru.iguana.deal.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.iguana.deal.api.dto.EmailMessageDto;
 import ru.iguana.deal.api.service.DocumentService;
-import ru.iguana.deal.kafka.KafkaProducer;
-import ru.iguana.deal.model.entity.Client;
-import ru.iguana.deal.model.entity.Statement;
-import ru.iguana.deal.model.entity.enums.EmailTheme;
-import ru.iguana.deal.model.repository.ClientRepository;
-import ru.iguana.deal.model.repository.StatementRepository;
+
 
 import java.util.UUID;
 
@@ -23,18 +18,21 @@ public class DocumentController {
     private final DocumentService documentService;
 
     @PostMapping("/send")
+    @Operation(summary = "Send documents", description = "Sends documents to the user for signing based on the provided statementId.")
     public void send(@PathVariable UUID statementId) {
         log.info("Received request to send documents for statementId: {}", statementId);
         documentService.sendDocuments(statementId);
     }
 
     @PostMapping("/sign")
+    @Operation(summary = "Sign documents", description = "Signs the user's documents based on the provided statementId.")
     public void sign(@PathVariable UUID statementId) {
         log.info("Received request to sign documents for statementId: {}", statementId);
         documentService.signDocuments(statementId);
     }
 
     @PostMapping("/code")
+    @Operation(summary = "Send SES code", description = "Sends an SES code to the user for document signing confirmation.")
     public void code(@PathVariable UUID statementId) {
         log.info("Received request to send SES code for statementId: {}", statementId);
         documentService.codeDocuments(statementId);
--- deal/src/main/java/ru/iguana/deal/api/dto/EmailMessageDto.java
+++ deal/src/main/java/ru/iguana/deal/api/dto/EmailMessageDto.java
@@ -1,5 +1,6 @@
 package ru.iguana.deal.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -11,13 +12,17 @@ import java.util.UUID;
 @Data
 @Accessors(chain = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Schema(description = "DTO for email message details")
 public class EmailMessageDto {
-
+    @Schema(description = "Recipient email address", example = "user@example.com")
     String address;
 
+    @Schema(description = "Email theme", example = "SEND_DOCUMENTS")
     EmailTheme theme;
 
+    @Schema(description = "Unique statement ID", example = "6bacc7ba-5568-4566-b0a5-8e324462b763")
     UUID statementId;
 
+    @Schema(description = "Email message content", example = "Please sign the documents")
     String text;
 }
