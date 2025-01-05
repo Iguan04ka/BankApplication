0ee17dfe446efd9bf479128167938c6aeef1bce7
Iguan04ka <Iguana_2004@mail.ru>
2025-01-05 21:34:37 +0400

    got email sending

--- dossier/src/main/java/ru/iguana/dossier/kafka/KafkaConsumer.java
+++ dossier/src/main/java/ru/iguana/dossier/kafka/KafkaConsumer.java
@@ -1,29 +1,39 @@
 package ru.iguana.dossier.kafka;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.iguana.dossier.service.MailSenderService;
 
 @Service
+@AllArgsConstructor
 public class KafkaConsumer {
+    private final MailSenderService mailSenderService;
+
+    private final MessageConvertor messageConvertor;
 
     @KafkaListener(topics = "finish-registration", groupId = "dossier")
     public void listenFinishRegistrationTopic(String message){
-        System.out.println(message);
+
     }
 
     @KafkaListener(topics = "send-documents", groupId = "dossier")
     public void listenSendDocumentsTopic(String message){
-        System.out.println(message);
+
     }
 
     @KafkaListener(topics = "create-documents", groupId = "dossier")
     public void listenSignDocumentsTopic(String message){
-        System.out.println(message);
+        
     }
 
     @KafkaListener(topics = "send-ses", groupId = "dossier")
     public void listenSendSesTopic(String message){
-        System.out.println(message.getClass());
+        System.out.println("Received message from Kafka: " + message);
+        mailSenderService.sendEmail(
+                messageConvertor.getAddress(message),
+                messageConvertor.getTheme(message),
+                messageConvertor.getText(message));
     }
 }
---
+++ dossier/src/main/java/ru/iguana/dossier/service/MailSenderService.java
@@ -0,0 +1,34 @@
+package ru.iguana.dossier.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailSenderService {
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, false);
+
+            javaMailSender.send(message);
+            System.out.println("Email sent successfully to: " + to);
+        } catch (MessagingException e) {
+            System.err.println("Failed to send email: " + e.getMessage());
+        }
+    }
+}
