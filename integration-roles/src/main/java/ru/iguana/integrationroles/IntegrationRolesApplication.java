c7974db1909194ebe29cfc84953f8f39ec3bfde1
Iguan04ka <Iguana_2004@mail.ru>
2025-07-05 14:28:37 +0400

    init integration-roles microservice

---
+++ integration-roles/src/main/java/ru/iguana/integrationroles/IntegrationRolesApplication.java
@@ -0,0 +1,13 @@
+package ru.iguana.integrationroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationRolesApplication.class, args);
+	}
+
+}
